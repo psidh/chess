cb632a3e9a5b374aeece32ab9539c3bb4a2ae4aa
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-12-23 14:01:51 +0530

    prod socket url updated
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- fullstack/src/hooks/useSocket.js
+++ fullstack/src/hooks/useSocket.js
@@ -18,16 +18,16 @@ export const useSocket = (type) => {
 
   useEffect(() => {
     if (email && type) {
-      // const ws = new WebSocket(
-      //   `wss://obliged-krystal-p-sidharth42069-c7350fbe.koyeb.app/?email=${encodeURIComponent(
-      //     email
-      //   )}&type=${type}`
-      // );
       const ws = new WebSocket(
-        `ws://localhost:3001/?email=${encodeURIComponent(
+        `wss://obliged-krystal-p-sidharth42069-c7350fbe.koyeb.app/?email=${encodeURIComponent(
           email
         )}&type=${type}`
       );
+      // const ws = new WebSocket(
+      //   `ws://localhost:3001/?email=${encodeURIComponent(
+      //     email
+      //   )}&type=${type}`
+      // );
       console.log(ws.url);
       console.log("Email: " + email);
 
